cb128cdf94435a45fe0150df01b83a78a0ccf95e
surajraul <surajraul98@gmail.com>
2024-10-25 18:02:06 +0530

    Folder Structure Change2

---
+++ src/services/LoginService/components/LoginService.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoginService = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LoginService
---
+++ src/services/RegistrationService/components/RegistrationService.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RegistrationService = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default RegistrationService
