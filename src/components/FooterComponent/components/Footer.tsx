6f3aa2c0c8cb282c9c1a6f625c9cd3ffd4b74de4
Rahul <rahulbuddies007@gmail.com>
2024-10-29 16:13:26 +0530

    Footer updated

--- src/components/FooterComponent/components/Footer.tsx
+++ src/components/FooterComponent/components/Footer.tsx
@@ -11,34 +11,26 @@ export const Footer: FC = (): ReactElement => {
         marginTop:"10px",
         paddingTop: "1rem",
         paddingBottom: "1rem",
-        position:"fixed",
+        position:"relative",
         bottom:0
       }}
     >
       <Container maxWidth="lg">
         <Grid2 container justifyContent="space-between" alignItems="center">
-          <Grid2 size={12}>
-            <Typography color="black" variant="h5">
-              Tiffin Service App
-            </Typography>
-          </Grid2>
-          <Grid2 size={12}>
-            <Typography color="textSecondary" variant="subtitle1">
-              {`@${new Date().getFullYear()} `}
-            </Typography>
-          </Grid2>
-          {/* About Us */}
-          <Grid2 size={{ xs: 12, md: 4}} textAlign={{ xs: "center", md: "left" }} sx={{ mt: 2 }}>
-            <Typography variant="body1" color="textPrimary">
-              <strong>About Us</strong>: We are dedicated to bringing delicious homemade meals straight to your doorstep.
-            </Typography> 
-          </Grid2>
-
           {/* Contact Information */}
-          <Grid2 size={{ xs: 12, md: 4 }} textAlign="center">
+          <Grid2 size={{ xs: 12, md: 4 }} textAlign="left">
             <Typography variant="body1" color="textPrimary">
               <strong>Contact:</strong> tiffinService@gmail.com | Phone: (123) 456-7890
             </Typography>
+          </Grid2>
+
+          <Grid2 size={{ xs: 12, md: 4}} textAlign={{ xs: "center", md: "center" }} sx={{ mt: 2 }}>
+            <Typography color="black" variant="h5">
+                Tiffin Service App
+              </Typography>
+              <Typography color="textSecondary" variant="subtitle1">
+                {`@${new Date().getFullYear()} `}
+              </Typography>
           </Grid2>
 
           {/* Social Media Links */}
