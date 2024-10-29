14b86bd9899cf90a74852fb7e6971816597ff456
surajraul <surajraul98@gmail.com>
2024-10-29 12:31:18 +0530

    Admin Registration committe

---
+++ src/components/NotFound/components/PageNotFound.tsx
@@ -0,0 +1,12 @@
+import { Box, Typography } from '@mui/material'
+import React from 'react'
+
+const PageNotFound = () => {
+  return (
+    <Box>
+        <Typography>Not Found</Typography>
+    </Box>
+  )
+}
+
+export default PageNotFound
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,35 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import NotFound from './components/NotFound';
+import AdminRegistrationPage from './pages/AdminRegistrationPage';
 
+let childRoutes = [
+  {
+    path: 'register',
+    element: <AdminRegistrationPage />
+  },
+  {
+    path: '**',
+    element: <NotFound></NotFound>
+  }
+]
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    children: childRoutes
+  }
+]);
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
+    {/* <App /> */}
   </React.StrictMode>
 );
 
